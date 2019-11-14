94306c4cf83542162ceba3a1f9b819f0a73ea414
jordan <jordan@jordans-Air.lan>
2019-11-14 22:04:04 +0700

    mvp met bug fixed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
-import Form from "./Form";
+import Form  from "./Form";
 
 function App() {
   return (
     <div className="App">
       <h1>Form :)</h1>
-      < Form />
+      <Form/>
     </div>
   );
 }
